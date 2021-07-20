ea515a42da8209bf67a1f777d56ce08d067edd1f
Christiano Green <christiano.green@orgafarma.com>
2021-07-20 09:30:14 -0300

    Update prune command added option 'type' and 'except' types

--- src/Console/PruneCommand.php
+++ src/Console/PruneCommand.php
@@ -8,13 +8,25 @@ use Sarfraznawaz2005\Meter\Models\MeterModel;
 
 class PruneCommand extends Command
 {
-    protected $signature = 'meter:prune {--days=1 : The number of days to retain Meter data}';
+    protected $signature = 'meter:prune {--days=1 : The number of days to retain Meter data} {--type=* : Data types to clean Meter data} {--except=* : Excepts types to clean Meter data}';
     protected $description = 'Prune stale entries from the Meter database';
 
     public function handle(MeterModel $model)
     {
         /* @var $model Builder */
-        $deleteCount = $model->whereDate('created_at', '<', now()->subDays($this->option('days')))->delete();
+        $query = $model->whereDate('created_at', '<', now()->subDays($this->option('days')));
+
+        if(count($this->option('type')))
+        {
+            $query->whereIn('type', $this->option('type'));
+        }
+
+        if(count($this->option('except')))
+        {
+            $query->whereNotIn('type', $this->option('except'));
+        }
+
+        $deleteCount = $query->delete();
 
         $this->info($deleteCount.' entries pruned.');
     }
